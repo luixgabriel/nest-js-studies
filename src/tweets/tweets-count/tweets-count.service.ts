db8126b52bc4dd71a238d82a5ca56785d717cd5e
luix gabriel <luisgasbriel@gmail.com>
2023-08-11 11:36:43 -0300

    chore: add docker and docker-compose

--- src/tweets/tweets-count/tweets-count.service.ts
+++ src/tweets/tweets-count/tweets-count.service.ts
@@ -1,17 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable} from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager/dist';
 import { InjectModel } from '@nestjs/sequelize';
 import { Tweet } from '../entities/tweet.entity';
 import {Interval} from '@nestjs/schedule'
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class TweetsCountService {
+    private limit: number = 10
     constructor(
         @InjectModel(Tweet)
-        private tweetModel: typeof Tweet
+        private tweetModel: typeof Tweet,
+        @Inject(CACHE_MANAGER)
+        private cacheManager: Cache,
+       
     ){}
 
     @Interval(5000)
     async countTweets(): Promise<void>{
-        
+        console.log('procurando tweets');
+        let offset = await this.cacheManager.get<number>('tweet-offset');
+        offset = offset === undefined ? 0 : offset;
+    
+        console.log(`offsets: ${offset}`);
+    
+        const tweets = await this.tweetModel.findAll({
+          offset,
+          limit: this.limit,
+        });
+    
+        console.log(`${tweets.length} encontrados`);
+    
+        if (tweets.length === this.limit) {
+          this.cacheManager.set('tweet-offset', offset + this.limit);
+          console.log(`achou + ${this.limit} tweets`);
+          // this.emailsQueue.add({ tweets: tweets.map((t) => t.toJSON()) });
+        }
+
+        }
     }
-}
+
+
--- src/tweets/tweets.module.ts
+++ src/tweets/tweets.module.ts
@@ -4,9 +4,11 @@ import { TweetsController } from './tweets.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Tweet } from './entities/tweet.entity';
 import { TweetsCountService } from './tweets-count/tweets-count.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Tweet])],
+  imports: [CacheModule.register(),
+    SequelizeModule.forFeature([Tweet])],
   controllers: [TweetsController],
   providers: [TweetsService, TweetsCountService],
 })
